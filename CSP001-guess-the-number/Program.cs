0ff82d63de1a8e84338e1f539514017e95b5bd03
connielc1 <constanzalatorrecid@gmail.com>
2024-04-29 17:59:30 -0400

    agregado de testing

---
+++ CSP001-guess-the-number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSP001_guess_the_number
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Antes de empezar, ingresa tu nombre:");
+            string playerName = Console.ReadLine() ?? "";
+
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                Game game = new Game(playerName);
+                game.Start();
+            }
+            else
+            {
+                Console.WriteLine("Nombre inválido. Reinicia el programa e intenta de nuevo.");
+            }
+        }
+    }
+}
